dd13fcceb7da30f089dd696e526f63a92b6ef590
osawa-koki <github@osawa.dev>
2023-03-22 23:04:12 +0900

    リファクタリング。

--- include/create_new_directory.hpp
+++ include/create_new_directory.hpp
@@ -7,17 +7,24 @@
 #include <cerrno>
 #include <cstdlib>
 
-void create_new_directory(const std::string& dir) {
+void create_new_directory(const std::string &dir)
+{
   struct stat st;
-  if (stat(dir.c_str(), &st) == 0) {
+  if (stat(dir.c_str(), &st) == 0)
+  {
     std::cerr << "Error: Directory '" << dir << "' already exists." << std::endl;
     exit(1);
-  } else if (errno == ENOENT) {
-    if (mkdir(dir.c_str(), 0777) != 0) {
+  }
+  else if (errno == ENOENT)
+  {
+    if (mkdir(dir.c_str(), 0777) != 0)
+    {
       std::cerr << "Error: Failed to create directory '" << dir << "': " << std::strerror(errno) << std::endl;
       exit(1);
     }
-  } else {
+  }
+  else
+  {
     std::cerr << "Error: Failed to check directory '" << dir << "': " << std::strerror(errno) << std::endl;
     exit(1);
   }
