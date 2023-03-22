cab937da7083a3a918370341eca9fd2b22cc5f6d
osawa-koki <github@osawa.dev>
2023-03-22 21:15:26 +0900

    小さな修正。

--- sample.sh
+++ sample.sh
@@ -2,4 +2,4 @@
 
 make clean && make
 rm -rf ./work/sample
-./bin/program -name sample -from 0 -upto 30 -aspect-ratio 1.5 -shrink-ratio 0.6 -filesize-height 100 -start-x -1.21990088 -start-y 0.335100082 -default-width 0.2 -default-height 0.15 -length 1800
+make run ARGS="-name sample -from 0 -upto 30 -aspect-ratio 1.5 -shrink-ratio 0.6 -filesize-height 100 -start-x -1.21990088 -start-y 0.335100082 -default-width 0.2 -default-height 0.15 -length 1800"
