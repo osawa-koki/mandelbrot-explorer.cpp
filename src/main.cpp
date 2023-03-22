3a40d3d65b74999156f7f53a0afd32a39a33c8a3
kosawa <Osawa.Koki@ecbeing.co.jp>
2023-03-22 14:57:46 +0900

    ファイル名の定義まで。

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "parse_commandline_args.hpp"
 #include "options.hpp"
 #include "create_new_directory.hpp"
+#include "zero_padding.hpp"
 
 #define OUTPUT_PRECISION 100
 
@@ -31,6 +32,12 @@ int main(int argc, char *argv[])
   // 対象のディレクトリを作成
   std::string target_directory = "./work/" + opt.name + "/";
   create_new_directory(target_directory);
+
+  // マンデルブロ集合の計算
+  for (int i = opt.from; i <= opt.upto; i++) {
+    std::string filename = target_directory + zero_padding(i, 8) + ".png";
+    std::cout << "FILENAME: " << filename << std::endl;
+  }
 
   return 0;
 }
