c81b289ee5e69d690512b110d6574104eac41bd6
osawa-koki <github@osawa.dev>
2023-03-22 22:48:04 +0900

    不要なパラメタ消した。

--- run.sh
+++ run.sh
@@ -11,7 +11,6 @@ START_X=-1.21765
 START_Y=0.3453625
 DEFAULT_WIDTH=0.2
 DEFAULT_HEIGHT=0.15
-LENGTH=1800
 
 # make clean && make コマンドを実行する
 make clean && make
@@ -19,6 +18,6 @@ make clean && make
 rm -rf ./work/$NAME
 
 # make run コマンドを実行する
-make run ARGS="-name $NAME -from $FROM -upto $UPTO -aspect-ratio $ASPECT_RATIO -shrink-ratio $SHRINK_RATIO -filesize-height $FILESIZE_HEIGHT -start-x $START_X -start-y $START_Y -default-width $DEFAULT_WIDTH -default-height $DEFAULT_HEIGHT -length $LENGTH"
+make run ARGS="-name $NAME -from $FROM -upto $UPTO -aspect-ratio $ASPECT_RATIO -shrink-ratio $SHRINK_RATIO -filesize-height $FILESIZE_HEIGHT -start-x $START_X -start-y $START_Y -default-width $DEFAULT_WIDTH -default-height $DEFAULT_HEIGHT"
 
 ffmpeg -r 30 -i ./work/$NAME/%08d.png -vcodec libx264 -pix_fmt yuv420p -r 60 ./dist/video.mp4
